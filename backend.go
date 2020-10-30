11a3a2df3761386c4c7a3cb9d60138558fecea79
KEYFACTOR\jkilgall <jd.kilgallin@css-security.com>
2020-10-30 15:05:46 -0400

    JSON encoding bug fixes, logging, cleanup

--- backend.go
+++ backend.go
@@ -25,14 +25,12 @@ import (
 	"github.com/hashicorp/vault/sdk/logical"
 )
 
-//var myClient *http.Client
 var config map[string]string
 var roles map[string]map[string]bool
 
 // Factory configures and returns backend
 func Factory(ctx context.Context, conf *logical.BackendConfig) (logical.Backend, error) {
 	confPath := os.Getenv("KF_CONF_PATH")
-	//	file, _ := ioutil.ReadFile("/root/vault/config/config-mar.json")
 	file, _ := ioutil.ReadFile(confPath)
 	config = make(map[string]string)
 	roles = make(map[string]map[string]bool)
@@ -57,7 +55,7 @@ func Factory(ctx context.Context, conf *logical.BackendConfig) (logical.Backend,
 	return b, nil
 }
 
-// backend wraps the backend framework and adds a map for storing key value pairs
+// Store certificates by serial number
 type backend struct {
 	*framework.Backend
 
@@ -111,6 +109,7 @@ func (b *backend) handleExistenceCheck(ctx context.Context, req *logical.Request
 	return out != nil, nil
 }
 
+// List all serial numbers known to engine
 func (b *backend) handleList(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
 	var list []string
 	for k, _ := range b.store {
@@ -120,13 +119,6 @@ func (b *backend) handleList(ctx context.Context, req *logical.Request, data *fr
 
 		list = append(list, strings.Split(k,"/")[1])
 	}
-//	content, _ := jsonutil.EncodeJSON(b.store)
-//	fmt.Println(content)
-//	b.Logger().Info("1"+string(content))
-//	var r map[string]interface{}
-//	jsonutil.DecodeJSON(content, &r)
-//	content, _ = jsonutil.EncodeJSON(r)
-//	b.Logger().Info("2"+string(content))
 	resp := &logical.Response{
 		Data: map[string]interface{} {
 			"keys":list,
@@ -136,87 +128,68 @@ func (b *backend) handleList(ctx context.Context, req *logical.Request, data *fr
 	return resp, nil
 }
 
-func (b *backend) load (ctx context.Context, req *logical.Request) {
-	data, _ := req.Storage.Get(ctx, "entry")
-	if data == nil {
-		return
-	}
-        var rawData map[string][]byte
-	jsonutil.DecodeJSON(data.Value, &rawData)
-	b.store = rawData
-}
-
+// Lookup certificate by serial number
 func (b *backend) handleRead(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-//	b.load(ctx, req)
-	if req.ClientToken == "" {
-		return nil, fmt.Errorf("client token empty")
-	}
-
+	// Get and canonicalize serial number from Vault path
 	path := data.Get("path").(string)
 	path = strings.ReplaceAll(path, "-","")
 	path = strings.ReplaceAll(path, ":","")
 	path = strings.ReplaceAll(path, "/cert/","")
 	b.Logger().Debug("requested " + path)
-	var rawData map[string]interface{}
-	serializedStore, err := jsonutil.EncodeJSON(b.store)
-	if err != nil {
-		return nil, err
-	}
-	b.Logger().Trace(string(serializedStore))
-	b.Logger().Debug(string(b.store[req.ClientToken+"/"+path]))
-	if err := jsonutil.DecodeJSON(b.store[req.ClientToken+"/"+path], &rawData); err != nil {
-		return nil, errwrap.Wrapf("json decoding failed: {{err}}", err)
-	}
+
+	// Lookup and decode certificate stored by given serial number
+	certBytes := b.store[path]
+	var certString string
+	jsonutil.DecodeJSON(certBytes,&certString)
+
+	// Conform response to Vault PKI API
 	resp := &logical.Response{
-		Data: rawData,
-		//		Data: map[string]interface{}{
-		//			"certificate":rawData[path],
-		//			"revocation_time":0,
-		//		},
+		Data: map[string]interface{}{
+			"certificate":certString,
+		},
 	}
 
 	return resp, nil
 }
 
+// Handle interface with crypto libraries and Keyfactor API to enroll a certificate with given content
 func (b *backend) submitCSR(host string, time string, template string, ca string, cn string, appkey string, creds string) ([]byte, []byte) {
-
+	// Generate keypair and CSR
 	keyBytes, _ := rsa.GenerateKey(rand.Reader, 2048)
-
 	subj := pkix.Name{
 		CommonName: cn,
 	}
 	rawSubj := subj.ToRDNSequence()
-
 	asn1Subj, _ := asn1.Marshal(rawSubj)
 	csrtemplate := x509.CertificateRequest{
 		RawSubject:         asn1Subj,
 		SignatureAlgorithm: x509.SHA256WithRSA,
 	}
 	csrBytes, _ := x509.CreateCertificateRequest(rand.Reader, &csrtemplate, keyBytes)
-
 	csrBuf := new(bytes.Buffer)
 	pem.Encode(csrBuf, &pem.Block{Type: "CERTIFICATE REQUEST", Bytes: csrBytes})
 
+	// This is only needed when running as a vault extension
 	b.Logger().Debug("Closing idle connections")
 	http.DefaultClient.CloseIdleConnections()
+
+	// Build request
 	url := config["protocol"] + "://" + host + "/KeyfactorAPI/Enrollment/CSR"
 	b.Logger().Debug("url: " + url)
-
 	bodyContent := "{\"CSR\": \"" + csrBuf.String() + "\",\"CertificateAuthority\":\"" + ca + "\",\"IncludeChain\": true, \"Metadata\": {}, \"Timestamp\": \"" + time + "\",\"Template\": \"" + template + "\",\"SANs\": {}}"
-
 	payload := strings.NewReader(bodyContent)
 	b.Logger().Debug("body: " + bodyContent)
 	req, err := http.NewRequest("POST", url, payload)
 	if err != nil {
 		b.Logger().Info("Error forming request: {{err}}", err)
 	}
-
 	req.Header.Add("x-keyfactor-requested-with", "APIClient")
 	req.Header.Add("content-type", "application/json")
 	req.Header.Add("x-keyfactor-appkey", appkey)
 	req.Header.Add("authorization", "Basic "+creds)
 	req.Header.Add("x-certificateformat", "PEM")
 
+	// Send request and check status
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		b.Logger().Info("Enrollment failed: {{err}}", err)
@@ -227,27 +200,28 @@ func (b *backend) submitCSR(host string, time string, template string, ca string
 		return nil, nil
 	}
 
+	// Read response and return certificate and key
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		b.Logger().Info("Error reading response: {{err}}", err)
 	}
-
 	b.Logger().Debug("Returning body: " + string(body))
 	return body, x509.MarshalPKCS1PrivateKey(keyBytes)
 }
+
 
 func (b *backend) requestCert(req *logical.Request, data *framework.FieldData, role string) (*logical.Response, error) {
 	location, _ := time.LoadLocation("UTC")
 	t := time.Now().In(location)
 	timestamp := t.Format("2006-01-02T15:04:05")
-
 	arg, _ := json.Marshal(req.Data)
-	b.Logger().Info(string(arg))
+	b.Logger().Debug(string(arg))
 	cn := ""
-	// Check to make sure that kv pairs provided
+
+	// Get and validate subject info from Vault command
 	if len(req.Data) == 0 {
-		return nil, fmt.Errorf("data must be provided to store in secret")
+		return nil, fmt.Errorf("common_name must be provided to issue certificate")
 	}
 	for k, v := range req.Data {
 		if k == "common_name" {
@@ -258,15 +232,16 @@ func (b *backend) requestCert(req *logical.Request, data *framework.FieldData, r
 		return nil, fmt.Errorf("Common name not allowed for provided role")
 	}
 
-	b.Logger().Debug("Connecting to " + config["host"] + "for common name " + cn)
+	// Call to generate and submit the CSR. TODO - simplify submitCSR params?
+	b.Logger().Debug("About to connect to " + config["host"] + "for common name " + cn)
 	result, key := b.submitCSR(config["host"], timestamp, config["template"], config["CA"], cn, config["appkey"], config["creds"])
 	if result == nil || key == nil {
 		return nil, fmt.Errorf("Could not enroll certificate")
 	}
 
+	// Parse response. TODO - move to submitCSR?
 	var r map[string]interface{}
 	json.Unmarshal(result, &r)
-
 	inner := r["CertificateInformation"].(map[string]interface{})
 	certI := inner["Certificates"].([]interface{})
 	certs := make([]string, len(certI))
@@ -277,8 +252,10 @@ func (b *backend) requestCert(req *logical.Request, data *framework.FieldData, r
 	b.Logger().Debug("Cert content: " + certs[0])
 	b.Logger().Debug("Serial number: " + serial)
 	buf := []byte(`{"` + serial + `":"` + certs[0] + `"}`)
-	b.store[req.ClientToken+"/"+serial] = buf
+	encoded, _ := jsonutil.EncodeJSON(buf)
+	b.store[serial] = encoded
 
+	// Conform response to Vault PKI API
 	response := &logical.Response{
 		Data: map[string]interface{}{
 			"certificate":      "-----BEGIN CERTIFICATE-----\n" + certs[0] + "\n-----END CERTIFICATE-----",
@@ -292,6 +269,7 @@ func (b *backend) requestCert(req *logical.Request, data *framework.FieldData, r
 	return response, nil
 }
 
+// Return true if a role is defined and false if not
 func (b *backend) checkRoleExists(role string) bool {
 	b.Logger().Trace("Checking role " + role + " against " + strconv.FormatInt(int64(len(roles)), 10) + " roles")
 	for k, _ := range roles {
@@ -303,8 +281,10 @@ func (b *backend) checkRoleExists(role string) bool {
 	return false
 }
 
+// Check if a domain is allowed for a given role based on allowed domains and whether subdomains are allowed
 func (b *backend) checkDomainAgainstRole(role string, domain string) bool {
 	for k, v := range roles[role] {
+		// If subdomains are allowed, only the suffix needs to match the allowed domain
 		if v && strings.HasSuffix(domain, k){
 			return true
 		}
@@ -315,27 +295,26 @@ func (b *backend) checkDomainAgainstRole(role string, domain string) bool {
 	return false
 }
 
-func (b *backend) save(ctx context.Context, req *logical.Request) {
-	data, _ := jsonutil.EncodeJSON(b.store)
-	entry := logical.StorageEntry {Key:"Data",Value:data}
-	req.Storage.Put(ctx, &entry)
-}
-
+// Add role or enroll certificate
 func (b *backend) handleWrite(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
 	// Break up path
 	path := strings.Split(data.Get("path").(string), "/")
 	b.Logger().Info(path[0])
 	b.Logger().Info(path[1])
-	// If issue, look up role then go to CSR
+	
+	// If issue, look up role then request certificate
 	if len(path) == 2 && path[0] == "issue" {
 		if !b.checkRoleExists(path[1]) {
 			return nil, fmt.Errorf("Cannot find provided role")
 		}
 		return b.requestCert(req, data, path[1])
 	}
+
 	// If roles, add role
 	if len(path) == 2 && path[0] == "roles" {
 		b.Logger().Trace("Adding role " + path[1])
+
+		// Parse role parameters
 		var domains []string
 		allowSubdomains := false
 		for k, v := range req.Data {
@@ -346,17 +325,20 @@ func (b *backend) handleWrite(ctx context.Context, req *logical.Request, data *f
 				allowSubdomains, _ = strconv.ParseBool(v.(string))
 			}
 		}
+
+		// Add the given role as a map where the keys are the domains and the values are whether to allow subdomains for that domain
 		roles[path[1]] = make(map[string]bool)
 		for i := range domains{
 			roles[path[1]][domains[i]] = allowSubdomains
 		}
 		roleString, _ := jsonutil.EncodeJSON(roles)
-		b.Logger().Info("roles: " + string(roleString))
+		b.Logger().Debug("roles: " + string(roleString))
 		return nil, nil
 	}
 	return nil, fmt.Errorf("Invalid path")
 }
 
+// Revoke certificate. TODO - change path
 func (b *backend) handleDelete(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
 	if req.ClientToken == "" {
 		return nil, fmt.Errorf("client token empty")
@@ -370,7 +352,7 @@ func (b *backend) handleDelete(ctx context.Context, req *logical.Request, data *
 	}
 
 	fmt.Println(path)
-	b.Logger().Info(string(b.store[req.ClientToken+"/"+path]))
+	b.Logger().Info(string(b.store[path]))
 
 	url := config["protocol"] + "://" + config["host"] + "/CMSAPI/Certificates/3/Revoke"
 	payload := `{"Lookup":{"Type":"Serial","SerialNumber":"` + path + `","IssuerDN":"CN=jdk-CA1,DC=jdk,DC=cms"},"Details":{"Reason":4, "EffectiveDate": "2020-5-5", "Comment":"" }}`
@@ -389,7 +371,7 @@ func (b *backend) handleDelete(ctx context.Context, req *logical.Request, data *
 	_, _ = ioutil.ReadAll(res.Body)
 
 	// Remove entry for specified path
-	delete(b.store, req.ClientToken+"/"+path)
+	delete(b.store, path)
 
 	return nil, nil
 }
