a85c981afbd05b32693195fb5c573ccdc10727cf
JoeKF <jvanwanzeele@keyfactor.com>
2022-10-28 13:50:03 -0400

    added license headers

--- unseal.sh
+++ unseal.sh
@@ -1,3 +1,20 @@
+__='
+   Copyright 2022 Keyfactor
+   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+   and limitations under the License.
+'
+
+Copyright 2022 Keyfactor
+   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+   and limitations under the License.
+'
+
 #!
 export VAULT_ADDR=http://127.0.0.1:8200
 vault operator unseal llTyznCImd8mMdAbKNOUHtkCn6kLMC5COJ+6sbCuJwqC && vault operator unseal TcWAJobiSBuNgRckPvu3jyILRaqZourQ6mwixSQktt+V && vault operator unseal ulHiSWHM8xV11nGbTdc3kHtWKKK0CxbqnOjJV3zXLXRI
