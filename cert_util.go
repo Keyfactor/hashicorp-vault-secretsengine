9bae23e2f54c64054aed558cee0f255701608b54
Joe VanWanzeele <jvanwanzeele@keyfactor.com>
2025-02-26 15:26:21 -0500

    updated to pass the metadata, dns_sans and ip_sans along with strict signing requests.

--- cert_util.go
+++ cert_util.go
@@ -28,10 +28,196 @@ import (
 	"strings"
 	"time"
 
+	"github.com/hashicorp/errwrap"
 	"github.com/hashicorp/vault/sdk/helper/errutil"
 	"github.com/hashicorp/vault/sdk/logical"
 	"go.mozilla.org/pkcs7"
 )
+
+// Generate keypair and CSR
+func (b *keyfactorBackend) generateCSR(cn string, ip_sans []string, dns_sans []string) (string, []byte) {
+	keyBytes, _ := rsa.GenerateKey(rand.Reader, 2048)
+	subj := pkix.Name{
+		CommonName: cn,
+	}
+	rawSubj := subj.ToRDNSequence()
+	asn1Subj, _ := asn1.Marshal(rawSubj)
+	var netIPSans []net.IP
+	for i := range ip_sans {
+		netIPSans = append(netIPSans, net.ParseIP(ip_sans[i]))
+	}
+
+	csrtemplate := x509.CertificateRequest{
+		RawSubject:         asn1Subj,
+		SignatureAlgorithm: x509.SHA256WithRSA,
+		IPAddresses:        netIPSans,
+		DNSNames:           dns_sans,
+	}
+	csrBytes, _ := x509.CreateCertificateRequest(rand.Reader, &csrtemplate, keyBytes)
+	csrBuf := new(bytes.Buffer)
+	pem.Encode(csrBuf, &pem.Block{Type: "CERTIFICATE REQUEST", Bytes: csrBytes})
+	return csrBuf.String(), x509.MarshalPKCS1PrivateKey(keyBytes)
+}
+
+// Handle interface with Keyfactor API to enroll a certificate with given content
+func (b *keyfactorBackend) submitCSR(ctx context.Context, req *logical.Request, csr string, caName string, templateName string, dns_sans []string, ip_sans []string, metaDataJson string) ([]string, string, error) {
+	config, err := b.fetchConfig(ctx, req.Storage)
+	if err != nil {
+		return nil, "", err
+	}
+	if config == nil {
+		return nil, "", errors.New("configuration is empty")
+	}
+
+	location, _ := time.LoadLocation("UTC")
+	t := time.Now().In(location)
+	time := t.Format("2006-01-02T15:04:05")
+
+	// get client
+	client, err := b.getClient(ctx, req.Storage)
+	if err != nil {
+		return nil, "", fmt.Errorf("error getting client: %w", err)
+	}
+
+	b.Logger().Debug("Closing idle connections")
+	client.httpClient.CloseIdleConnections()
+
+	// build request parameter structure
+
+	// build dns_sans payload string
+	dns_sans_payload_string := ""
+
+	for _, d := range dns_sans {
+		if d != dns_sans[0] {
+			dns_sans_payload_string += "," // pre-pend a comma before next entry if not the first entry
+		}
+		dns_sans_payload_string = dns_sans_payload_string + fmt.Sprintf("\"%s\"", d)
+	}
+	b.Logger().Debug("dns_sans payload string = %s", dns_sans_payload_string)
+
+	ip_sans_payload_string := ""
+
+	for _, i := range ip_sans {
+		if i != ip_sans[0] {
+			ip_sans_payload_string += ","
+		}
+		ip_sans_payload_string = ip_sans_payload_string + fmt.Sprintf("\"%s\"", i)
+	}
+	b.Logger().Debug("ip_sans payload string = %s", ip_sans_payload_string)
+
+	url := config.KeyfactorUrl + "/" + config.CommandAPIPath + "/Enrollment/CSR"
+	b.Logger().Debug("url: " + url)
+	bodyContent := "{\"CSR\": \"" + csr + "\", \"CertificateAuthority\":\"" + caName + "\", \"IncludeChain\": true, \"Metadata\": " + metaDataJson + ", \"Timestamp\": \"" + time + "\",\"Template\": \"" + templateName + "\""
+
+	sans_payload := "\"SANs\": {"
+
+	if dns_sans_payload_string != "" || ip_sans_payload_string != "" {
+		if dns_sans_payload_string != "" {
+			sans_payload += "\"dns\": [" + dns_sans_payload_string + "]"
+		}
+		if ip_sans_payload_string != "" {
+			sans_payload += ", \"ip\": [" + ip_sans_payload_string + "]"
+		}
+	}
+	sans_payload += "}"
+
+	b.Logger().Trace(fmt.Sprintf("sans_payload: %s", sans_payload))
+	bodyContent += ", " + sans_payload + "}"
+	payload := strings.NewReader(bodyContent)
+
+	b.Logger().Debug("request body: " + bodyContent)
+	httpReq, err := http.NewRequest("POST", url, payload)
+
+	if err != nil {
+		b.Logger().Info("Error forming request: {{err}}", err)
+	}
+
+	httpReq.Header.Add("x-keyfactor-requested-with", "APIClient")
+	httpReq.Header.Add("content-type", "application/json")
+	httpReq.Header.Add("x-certificateformat", "PEM")
+
+	// Send request and check status
+
+	b.Logger().Debug("About to connect to " + config.KeyfactorUrl + "for csr submission")
+	res, err := client.httpClient.Do(httpReq)
+	if err != nil {
+		b.Logger().Info("CSR Enrollment failed: {{err}}", err.Error())
+		return nil, "", err
+	}
+	if res.StatusCode != 200 {
+		b.Logger().Error("CSR Enrollment failed: server returned" + fmt.Sprint(res.StatusCode))
+		defer res.Body.Close()
+		body, _ := io.ReadAll(res.Body)
+		b.Logger().Error("Error response: " + string(body[:]))
+		return nil, "", fmt.Errorf("CSR Enrollment request failed with status code %d and error: "+string(body[:]), res.StatusCode)
+	}
+
+	// Read response and return certificate and key
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		b.Logger().Error("Error reading response: {{err}}", err)
+		return nil, "", err
+	}
+
+	// Parse response
+	var r map[string]interface{}
+	json.Unmarshal(body, &r)
+	b.Logger().Debug("response = ", r)
+
+	inner := r["CertificateInformation"].(map[string]interface{})
+	certI := inner["Certificates"].([]interface{})
+	certs := make([]string, len(certI))
+	for i, v := range certI {
+		certs[i] = v.(string)
+		start := strings.Index(certs[i], "-----BEGIN CERTIFICATE-----")
+		certs[i] = certs[i][start:]
+	}
+	serial := inner["SerialNumber"].(string)
+	kfId := inner["KeyfactorID"].(float64)
+
+	b.Logger().Debug("parsed response: ", certI...)
+
+	if err != nil {
+		b.Logger().Error("unable to parse ca_chain response", fmt.Sprint(err))
+	}
+	caEntry, err := logical.StorageEntryJSON("ca_chain/", certs[1:])
+	if err != nil {
+		b.Logger().Error("error creating ca_chain entry", err)
+	}
+
+	err = req.Storage.Put(ctx, caEntry)
+	if err != nil {
+		b.Logger().Error("error storing the ca_chain locally", err)
+	}
+
+	key := "certs/" + normalizeSerial(serial)
+
+	entry := &logical.StorageEntry{
+		Key:   key,
+		Value: []byte(certs[0]),
+	}
+
+	b.Logger().Debug("cert entry.Value = ", string(entry.Value))
+
+	err = req.Storage.Put(ctx, entry)
+	if err != nil {
+		return nil, "", errwrap.Wrapf("unable to store certificate locally: {{err}}", err)
+	}
+
+	kfIdEntry, err := logical.StorageEntryJSON("kfId/"+normalizeSerial(serial), kfId)
+	if err != nil {
+		return nil, "", err
+	}
+
+	err = req.Storage.Put(ctx, kfIdEntry)
+	if err != nil {
+		return nil, "", errwrap.Wrapf("unable to store the keyfactor ID for the certificate locally: {{err}}", err)
+	}
+
+	return certs, serial, nil
+}
 
 // fetch the CA info from keyfactor
 func fetchCAInfo(ctx context.Context, req *logical.Request, b *keyfactorBackend, caName string, includeChain bool) (response *logical.Response, retErr error) {
@@ -146,6 +332,130 @@ func fetchCAInfo(ctx context.Context, req *logical.Request, b *keyfactorBackend,
 	return resp, nil
 }
 
+// Allows fetching certificates from the backend; it handles the slightly
+// separate pathing for CA and revoked certificates.
+func fetchCertBySerial(ctx context.Context, req *logical.Request, prefix, serial string) (*logical.StorageEntry, error) {
+	var path, legacyPath string
+	var err error
+	var certEntry *logical.StorageEntry
+
+	hyphenSerial := normalizeSerial(serial)
+	colonSerial := strings.Replace(strings.ToLower(serial), "-", ":", -1)
+
+	switch {
+	// Revoked goes first as otherwise ca/crl get hardcoded paths which fail if
+	// we actually want revocation info
+	case strings.HasPrefix(prefix, "revoked/"):
+		legacyPath = "revoked/" + colonSerial
+		path = "revoked/" + hyphenSerial
+	default:
+		legacyPath = "certs/" + colonSerial
+		path = "certs/" + hyphenSerial
+	}
+
+	certEntry, err = req.Storage.Get(ctx, path)
+	if err != nil {
+		return nil, errutil.InternalError{Err: fmt.Sprintf("error fetching certificate %s: %s", serial, err)}
+	}
+	if certEntry != nil {
+		if len(certEntry.Value) == 0 {
+			return nil, errutil.InternalError{Err: fmt.Sprintf("returned certificate bytes for serial %s were empty", serial)}
+		}
+		return certEntry, nil
+	}
+
+	// If legacyPath is unset, it's going to be a CA or CRL; return immediately
+	if legacyPath == "" {
+		return nil, nil
+	}
+
+	// Retrieve the old-style path.  We disregard errors here because they
+	// always manifest on windows, and thus the initial check for a revoked
+	// cert fails would return an error when the cert isn't revoked, preventing
+	// the happy path from working.
+	certEntry, _ = req.Storage.Get(ctx, legacyPath)
+	if certEntry == nil {
+		return nil, nil
+	}
+	if len(certEntry.Value) == 0 {
+		return nil, errutil.InternalError{Err: fmt.Sprintf("returned certificate bytes for serial %s were empty", serial)}
+	}
+
+	// Update old-style paths to new-style paths
+	certEntry.Key = path
+	if err = req.Storage.Put(ctx, certEntry); err != nil {
+		return nil, errutil.InternalError{Err: fmt.Sprintf("error saving certificate with serial %s to new location", serial)}
+	}
+	if err = req.Storage.Delete(ctx, legacyPath); err != nil {
+		return nil, errutil.InternalError{Err: fmt.Sprintf("error deleting certificate with serial %s from old location", serial)}
+	}
+
+	return certEntry, nil
+}
+
+func fetchCertIssuedByCA(ctx context.Context, req *logical.Request, b *keyfactorBackend, caName string) (KeyfactorCertResponse, error) {
+	// call certificates endpoint, limit results to 1, filter by CA name
+	config, err := b.fetchConfig(ctx, req.Storage)
+	if err != nil {
+		return nil, err
+	}
+	if config == nil {
+		return nil, errors.New("unable to load configuration")
+	}
+
+	// get the client
+	client, err := b.getClient(ctx, req.Storage)
+	if err != nil {
+		b.Logger().Error("unable to create the http client")
+	}
+	// This is only needed when running as a vault extension
+	b.Logger().Debug("Closing idle connections")
+	client.httpClient.CloseIdleConnections()
+
+	url := config.KeyfactorUrl + "/" + config.CommandAPIPath + "/Certificates?pq.queryString=CA%20-eq%20%22" + caName + "%22%20&ReturnLimit=1"
+	b.Logger().Debug("url: " + url)
+
+	httpReq, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		b.Logger().Info("Error forming request: {{err}}", err)
+	}
+	httpReq.Header.Add("x-keyfactor-requested-with", "APIClient")
+	httpReq.Header.Add("content-type", "application/json")
+
+	// Send request and check status
+	b.Logger().Debug("About to connect to " + config.KeyfactorUrl + "for cert retrieval")
+	res, err := client.httpClient.Do(httpReq)
+	if err != nil {
+		b.Logger().Info("failed getting cert: {{err}}", err)
+		return nil, err
+	}
+	if res.StatusCode != 200 {
+		b.Logger().Error("request failed: server returned" + fmt.Sprint(res.StatusCode))
+		b.Logger().Error("Error response = " + fmt.Sprint(res.Body))
+		return nil, fmt.Errorf("error downloading certificate. returned status = %d\n ", res.StatusCode)
+	}
+
+	// Read response and return certificate and key
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		b.Logger().Info("Error reading response: {{err}}", err)
+		return nil, err
+	}
+
+	// Parse response
+	var r KeyfactorCertResponse
+	json.Unmarshal(body, &r)
+	b.Logger().Debug("response = ", r)
+
+	if len(r) == 0 {
+		return nil, fmt.Errorf("no certificates issued by CA %s found in Command.  At least 1 must exist in order to retreive the CA or CA chain certificate(s)", caName)
+	}
+
+	return r, nil
+}
+
 func fetchChainAndCAForCert(ctx context.Context, req *logical.Request, b *keyfactorBackend, kfCertId int) ([]string, string, error) {
 	config, err := b.fetchConfig(ctx, req.Storage)
 	if err != nil {
@@ -238,180 +548,8 @@ func fetchChainAndCAForCert(ctx context.Context, req *logical.Request, b *keyfac
 	return ca_chain_pem, ca_pem, nil
 }
 
-// Generate keypair and CSR
-func (b *keyfactorBackend) generateCSR(cn string, ip_sans []string, dns_sans []string) (string, []byte) {
-	keyBytes, _ := rsa.GenerateKey(rand.Reader, 2048)
-	subj := pkix.Name{
-		CommonName: cn,
-	}
-	rawSubj := subj.ToRDNSequence()
-	asn1Subj, _ := asn1.Marshal(rawSubj)
-	var netIPSans []net.IP
-	for i := range ip_sans {
-		netIPSans = append(netIPSans, net.ParseIP(ip_sans[i]))
-	}
-
-	csrtemplate := x509.CertificateRequest{
-		RawSubject:         asn1Subj,
-		SignatureAlgorithm: x509.SHA256WithRSA,
-		IPAddresses:        netIPSans,
-		DNSNames:           dns_sans,
-	}
-	csrBytes, _ := x509.CreateCertificateRequest(rand.Reader, &csrtemplate, keyBytes)
-	csrBuf := new(bytes.Buffer)
-	pem.Encode(csrBuf, &pem.Block{Type: "CERTIFICATE REQUEST", Bytes: csrBytes})
-	return csrBuf.String(), x509.MarshalPKCS1PrivateKey(keyBytes)
-}
-
-// Allows fetching certificates from the backend; it handles the slightly
-// separate pathing for CA and revoked certificates.
-func fetchCertBySerial(ctx context.Context, req *logical.Request, prefix, serial string) (*logical.StorageEntry, error) {
-	var path, legacyPath string
-	var err error
-	var certEntry *logical.StorageEntry
-
-	hyphenSerial := normalizeSerial(serial)
-	colonSerial := strings.Replace(strings.ToLower(serial), "-", ":", -1)
-
-	switch {
-	// Revoked goes first as otherwise ca/crl get hardcoded paths which fail if
-	// we actually want revocation info
-	case strings.HasPrefix(prefix, "revoked/"):
-		legacyPath = "revoked/" + colonSerial
-		path = "revoked/" + hyphenSerial
-	default:
-		legacyPath = "certs/" + colonSerial
-		path = "certs/" + hyphenSerial
-	}
-
-	certEntry, err = req.Storage.Get(ctx, path)
-	if err != nil {
-		return nil, errutil.InternalError{Err: fmt.Sprintf("error fetching certificate %s: %s", serial, err)}
-	}
-	if certEntry != nil {
-		if len(certEntry.Value) == 0 {
-			return nil, errutil.InternalError{Err: fmt.Sprintf("returned certificate bytes for serial %s were empty", serial)}
-		}
-		return certEntry, nil
-	}
-
-	// If legacyPath is unset, it's going to be a CA or CRL; return immediately
-	if legacyPath == "" {
-		return nil, nil
-	}
-
-	// Retrieve the old-style path.  We disregard errors here because they
-	// always manifest on windows, and thus the initial check for a revoked
-	// cert fails would return an error when the cert isn't revoked, preventing
-	// the happy path from working.
-	certEntry, _ = req.Storage.Get(ctx, legacyPath)
-	if certEntry == nil {
-		return nil, nil
-	}
-	if len(certEntry.Value) == 0 {
-		return nil, errutil.InternalError{Err: fmt.Sprintf("returned certificate bytes for serial %s were empty", serial)}
-	}
-
-	// Update old-style paths to new-style paths
-	certEntry.Key = path
-	if err = req.Storage.Put(ctx, certEntry); err != nil {
-		return nil, errutil.InternalError{Err: fmt.Sprintf("error saving certificate with serial %s to new location", serial)}
-	}
-	if err = req.Storage.Delete(ctx, legacyPath); err != nil {
-		return nil, errutil.InternalError{Err: fmt.Sprintf("error deleting certificate with serial %s from old location", serial)}
-	}
-
-	return certEntry, nil
-}
-
-func parseOtherSANs(others []string) (map[string][]string, error) {
-	result := map[string][]string{}
-	for _, other := range others {
-		splitOther := strings.SplitN(other, ";", 2)
-		if len(splitOther) != 2 {
-			return nil, fmt.Errorf("expected a semicolon in other SAN %q", other)
-		}
-		splitType := strings.SplitN(splitOther[1], ":", 2)
-		if len(splitType) != 2 {
-			return nil, fmt.Errorf("expected a colon in other SAN %q", other)
-		}
-		switch {
-		case strings.EqualFold(splitType[0], "utf8"):
-		case strings.EqualFold(splitType[0], "utf-8"):
-		default:
-			return nil, fmt.Errorf("only utf8 other SANs are supported; found non-supported type in other SAN %q", other)
-		}
-		result[splitOther[0]] = append(result[splitOther[0]], splitType[1])
-	}
-
-	return result, nil
-}
-
 func normalizeSerial(serial string) string {
 	return strings.Replace(strings.ToLower(serial), ":", "-", -1)
-}
-
-func fetchCertIssuedByCA(ctx context.Context, req *logical.Request, b *keyfactorBackend, caName string) (KeyfactorCertResponse, error) {
-	// call certificates endpoint, limit results to 1, filter by CA name
-	config, err := b.fetchConfig(ctx, req.Storage)
-	if err != nil {
-		return nil, err
-	}
-	if config == nil {
-		return nil, errors.New("unable to load configuration")
-	}
-
-	// get the client
-	client, err := b.getClient(ctx, req.Storage)
-	if err != nil {
-		b.Logger().Error("unable to create the http client")
-	}
-	// This is only needed when running as a vault extension
-	b.Logger().Debug("Closing idle connections")
-	client.httpClient.CloseIdleConnections()
-
-	url := config.KeyfactorUrl + "/" + config.CommandAPIPath + "/Certificates?pq.queryString=CA%20-eq%20%22" + caName + "%22%20&ReturnLimit=1"
-	b.Logger().Debug("url: " + url)
-
-	httpReq, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		b.Logger().Info("Error forming request: {{err}}", err)
-	}
-	httpReq.Header.Add("x-keyfactor-requested-with", "APIClient")
-	httpReq.Header.Add("content-type", "application/json")
-
-	// Send request and check status
-	b.Logger().Debug("About to connect to " + config.KeyfactorUrl + "for cert retrieval")
-	res, err := client.httpClient.Do(httpReq)
-	if err != nil {
-		b.Logger().Info("failed getting cert: {{err}}", err)
-		return nil, err
-	}
-	if res.StatusCode != 200 {
-		b.Logger().Error("request failed: server returned" + fmt.Sprint(res.StatusCode))
-		b.Logger().Error("Error response = " + fmt.Sprint(res.Body))
-		return nil, fmt.Errorf("error downloading certificate. returned status = %d\n ", res.StatusCode)
-	}
-
-	// Read response and return certificate and key
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		b.Logger().Info("Error reading response: {{err}}", err)
-		return nil, err
-	}
-
-	// Parse response
-	var r KeyfactorCertResponse
-	json.Unmarshal(body, &r)
-	b.Logger().Debug("response = ", r)
-
-	if len(r) == 0 {
-		return nil, fmt.Errorf("no certificates issued by CA %s found in Command.  At least 1 must exist in order to retreive the CA or CA chain certificate(s)", caName)
-	}
-
-	return r, nil
 }
 
 // ConvertBase64P7BtoCertificates takes a base64 encoded P7B certificate string and returns a slice of *x509.Certificate.
